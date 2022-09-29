faea1da7d39fe962895a204fa515056e4dc7d2ad
Matthew Petrin <matthewgpetrin@gmail.com>
2022-09-28 23:42:43 -0400

    Homework 2 directory added

---
+++ homework-2/bugs/12fpcount.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+//  Original:
+//  int main() {
+//  	float sum = 0;
+//  	for (float i = 1; i <= 100000000.0f; i++)
+//  		sum += i;
+//  	return sum;
+//  }
+
+// Corrected:
+int main() {
+ 	float int sum = 0;
+  	for (float int i = 1; i <= 100000000.0f; i++)
+  		sum += i;
+  	return sum;
+}
